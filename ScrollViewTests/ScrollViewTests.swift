14e1151de14868e8eb500af482c8c292506f149e
MacOS <ahmedawadallah@MacBook-Air.local>
2024-12-28 12:37:36 +0200

    upload project file

---
+++ ScrollViewTests/ScrollViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewTests.swift
+//  ScrollViewTests
+//
+//  Created by MacOS on 28/12/2024.
+//
+
+import Testing
+@testable import ScrollView
+
+struct ScrollViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
